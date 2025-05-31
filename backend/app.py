bc5f8bd4530b30d7589c8e6adbb346d54232e394
uylulu <uy.do.trong@gmail.com>
2025-05-31 15:59:12 +0800

    added frontend url for CORS

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 CORS(app, resources={
     r"/api/*": {
-        "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
+        "origins": ["https://cheerful-cocada-8192f4.netlify.app", "http://localhost:3000", "http://127.0.0.1:3000"],
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type"]
     }
